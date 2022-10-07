2d43a9155dff81714f5f034126e344bbfabdf140
Your Name <you@example.com>
2022-10-07 16:30:30 -0300

    first commit

---
+++ src/main/java/com/apipagamento/apipagamento/ApipagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.apipagamento.apipagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipagamentoApplication.class, args);
+	}
+
+}
